147d483848098730068454d045535cdcb1796881
bocman17 <hejlikt@gmail.com>
2022-02-28 23:53:35 +0100

    update token

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -5,7 +5,10 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+  mode: 'no-cors',
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  },
 })
 
 // Get search results
